f7246b79fa81b137e42eced26f4d986dc6be547c
futjikato <m.spindelhirn@cashiers-check.de>
2014-01-20 18:54:20 +0100

    Refactoring of app.js Added lazy loading ( loading 8 tracks at a time )

--- app.js
+++ app.js
@@ -1,49 +1,131 @@
+var sounds = {},
+    runningSound,
+    trackIds = {},
+    limit = 8,
+    offset = 0;
+
+var template = $('.sc-track-template').html(),
+    row = $('.sc-container');
+
+// initialize soundcloud api
 SC.initialize({
     client_id: "552867ff6d8ab877d1dad436413dc4f4"
 });
 
-$(function() {
-    var template = $('.sc-track-template').html(),
-        row = $('.sc-container');
+// load playlist detail information
+SC.get("/playlists/15347027", {limit: 0}, function(data){
+    // we want to load the oldest one first
+    offset = data.track_count - limit;
+    console.log('inital offset', offset);
+    enableLazyLoading();
+});
 
-    function addTrack(track) {
-        var newTrack = Mustache.render(template, track);
-        row.append(newTrack);
-        $('.js-img-init').each(function() {
-            $(this).attr('src', $(this).data('url')).removeAttr('js-img-init');
-        })
+/**
+ * Checks if an element is in the viewport
+ * See http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport/7557433#7557433
+ *
+ * @param el
+ * @returns {boolean}
+ */
+function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
+/**
+ * Converts a time given in milliseconds to a human readable duration string
+ * e.g. 04:45
+ *
+ * @param milliseconds
+ * @returns {string}
+ */
+function getDuration(milliseconds) {
+    var seconds = parseInt(milliseconds / 1000),
+        minutes = Math.floor(seconds / 60);
+    if(minutes<10) minutes = "0" + minutes;
+    seconds = seconds % 60;
+    if(seconds<10) seconds = "0" + seconds;
+    return minutes + ":" + seconds;
+}
+
+var isLoading = false;
+/**
+ * Returns the change handler for lazy loading
+ *
+ * @param el
+ * @returns {Function}
+ */
+function elementVisibilityMayChange (el) {
+    return function () {
+        if ( isElementInViewport(el) && !isLoading) {
+            loadTracks();
+        }
+    }
+}
+
+
+/**
+ * Load the next 20 tracks the street walking music playlist on soundcloud
+ */
+function loadTracks() {
+    if(offset < -limit) {
+        console.log("disable loading", offset);
+        $('.sc-loading').hide();
+        return;
     }
 
-    // todo implement limit & offset to lazy load tracks ( currently only 13 in playlist so ... )
-    SC.get("/playlists/15347027/tracks", {}, function(tracks){
-        $('.sc-loading').hide();
+    isLoading = true;
+    SC.get("/playlists/15347027/tracks", {limit: limit, offset: ((offset > 0) ? offset : 0)}, function(tracks){
         // show in reverse order
         tracks.reverse();
         $.each(tracks, function(i, track) {
-            console.log(track);
+            offset--;
+            console.log("decreasing offset", offset);
             addTrack(track);
         });
+
+        isLoading = false;
+        handler();
     });
+}
 
-    /**
-     * Converts a time given in milliseconds to a human readable duration string
-     * e.g. 04:45
-     *
-     * @param milliseconds
-     * @returns {string}
-     */
-    function getDuration(milliseconds) {
-        var seconds = parseInt(milliseconds / 1000),
-            minutes = Math.floor(seconds / 60);
-        if(minutes<10) minutes = "0" + minutes;
-        seconds = seconds % 60;
-        if(seconds<10) seconds = "0" + seconds;
-        return minutes + ":" + seconds;
-    }
+/**
+ * Adds a track to the site.
+ * The given datastructure is given by the soundcloud api
+ *
+ * @param track
+ */
+function addTrack(track) {
+    // do never add duplicates ( can happen at the end of the playlist because of offset <-> limit calculation )
+    if(trackIds[track.id])
+        return;
 
-    var sounds = {},
-        runningSound;
+    // add track id to list
+    trackIds[track.id] = true;
 
+    var newTrack = Mustache.render(template, track);
+    row.append(newTrack);
+    $('.js-img-init').each(function() {
+        $(this).attr('src', $(this).data('url')).removeAttr('js-img-init');
+    })
+}
+
+var handler = elementVisibilityMayChange($('.sc-loading').get(0));
+/**
+ * Enables lazy loading by binding the handler to some relevant window events.
+ */
+function enableLazyLoading() {
+    handler();
+    $(window).on('DOMContentLoaded load resize scroll', handler);
+}
+
+$(function() {
     /**
      * Handle click events on the play and pause button.
      *
@@ -69,7 +151,7 @@ $(function() {
             SC.stream("/tracks/" + trackid, {
                 whileloading: function() {
                     var percentage = Math.ceil(100 / this.bytesTotal * this.bytesLoaded);
-                    trackContainer.find('.progress>.meter.sc-loading').css('width', percentage + "%");
+                    trackContainer.find('.progress>.meter.sc-track-loading').css('width', percentage + "%");
                     trackContainer.find('.sc-timing-total').removeClass('hide').text(getDuration(this.durationEstimate));
                 },
                 onplay: function() {
@@ -89,7 +171,7 @@ $(function() {
                 },
                 whileplaying: function() {
                     var percentage = Math.ceil(100 / this.duration * this.position);
-                    trackContainer.find('.progress>.meter.sc-playing').css('width', percentage + "%");
+                    trackContainer.find('.progress>.meter.sc-track-playing').css('width', percentage + "%");
                     trackContainer.find('.sc-timing-running').text(getDuration(this.position));
                 },
                 onload: function() {
@@ -97,7 +179,7 @@ $(function() {
                 },
                 onfinish: function() {
                     trackContainer.removeClass('playing').addClass('stopped');
-                    trackContainer.find('.progress>.meter.sc-playing').css('width', 0 + "%");
+                    trackContainer.find('.progress>.meter.sc-track-playing').css('width', 0 + "%");
                     this.setPosition(0);
 
                     var next = trackContainer.next();
